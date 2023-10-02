9294cf81d66cfb95e151c7cf8be254bcbfaf88f6
Damola Olusakin <olusakinadedamola@gmail.com>
2023-10-02 14:18:35 -0400

    first commit

---
+++ main_app/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Car(models.Model):
+    make = models.CharField(max_length=50)
+    model = models.CharField(max_length=50)
+    color = models.CharField(max_length=50)
+    licence = models.CharField(max_length=10)
+
+class Service(models.Model):
+    service_name = models.CharField(max_length=50)
+    service_description = models.TextField(max_length=200)
+    service_price = models.IntegerField
+    service_duration = models.CharField(max_length=20)
